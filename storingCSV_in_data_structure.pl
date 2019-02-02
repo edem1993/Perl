9f84119bb50e815a2237a292511fd38d5b85bb1d
edem1993 <denesa1993@gmail.com>
2019-02-02 20:10:37 +0100

    1235

--- storingCSV_in_data_structure.pl
+++ storingCSV_in_data_structure.pl
@@ -25,8 +25,9 @@ sub main {
         # print "'$line'\n";
 
         # match 0 or more spaces
+        # make values for keys
         my ($name, $payment, $date) = split /\s*,\s*/, $line;
-
+        # makes %values hash with the values called
         my %values = (
             "Name" => $name,
             "Payment" => $payment,
@@ -38,6 +39,10 @@ sub main {
 
     close(INPUT);
 
+    # uses [rows] and {"hahs_values"}
+	#print $data[1]{"Date"} . "\n";
+
+    # references the "Payment" key from the hash!
     foreach my $data(@data) {
         print $data->{"Payment"} . "\n";
 
